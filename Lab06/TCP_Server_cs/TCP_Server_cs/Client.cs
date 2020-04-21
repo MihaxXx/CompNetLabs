9da7d8c8c9c2b5d06543b61e6e99e351ad648bce
Mikhail <mikes22@yandex.ru>
2020-04-21 23:06:17 +0300

    Add base for Lab06: CS-TCP Rb-WS

---
+++ Lab06/TCP_Server_cs/TCP_Server_cs/Client.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Net;      // потребуется
+using System.Net.Sockets;    // потребуется
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+class Client : IDisposable
+{
+    NetworkStream s;
+    TcpClient client;
+
+
+    public Client(TcpClient c)
+    {
+        s = c.GetStream();
+        client = c;
+    }
+
+    public void Dispose()
+    {
+        s.Dispose();
+    }
+
+    async Task<byte[]> ReadFromStreamAsync(int nbytes)
+    {
+        var buf = new byte[nbytes];
+        var readpos = 0;
+        while (readpos < nbytes)
+            readpos += await s.ReadAsync(buf, readpos, nbytes - readpos);
+        return buf;
+    }
+
+    public async Task ProcessAsync()
+    {
+        while (client.Connected)
+        {
+            //var actionBuffer = await ReadFromStreamAsync(2);
+            byte[] msg = new byte[1024];     // готовим место для принятия сообщения
+            int count = await s.ReadAsync(msg, 0, msg.Length);   // читаем сообщение от клиента
+            Console.Write(Encoding.Default.GetString(msg, 0, count)); // выводим на экран полученное сообщение в виде строки
+        }
+    }
+    async Task<string> ReadWithTimeout(int n) // (*)
+    {
+        using (var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(1000)))
+        {
+            try
+            {
+                return Decode(await ReadFromStreamAsync(n, cts.Token));
+            }
+            catch (OperationCanceledException)
+            {
+                return null;
+            }
+        }
+    }
+
+    private string Decode(byte[] msg)
+    {
+        return Encoding.Default.GetString(msg);
+    }
+
+    async Task<byte[]> ReadFromStreamAsync(int nbytes, CancellationToken ct)
+    {
+        var buf = new byte[nbytes];
+        var readpos = 0;
+        while (readpos < nbytes)
+            readpos += await s.ReadAsync(buf, readpos, nbytes - readpos, ct);
+        return buf;
+    }
+}
---
+++ Lab06/TCP_Server_cs/TCP_Server_cs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Net;      // потребуется
+using System.Net.Sockets;    // потребуется
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        RunServer();
+        Console.ReadLine();
+    }
+    static async void RunServer()
+    {
+        var tcpListener = TcpListener.Create(1234);
+        tcpListener.Start();
+        while (true)
+        {
+            var tcpClient = await tcpListener.AcceptTcpClientAsync();
+            _ = processClient(tcpClient); // await не нужен
+        }
+    }
+
+    static HashSet<Task> activeClientTasks = new HashSet<Task>();
+
+    static async Task processClient(TcpClient c)
+    {
+        using (var client = new Client(c))
+        {
+            Task task = client.ProcessAsync();
+            activeClientTasks.Add(task);
+            await task;
+        }
+    }
+}
