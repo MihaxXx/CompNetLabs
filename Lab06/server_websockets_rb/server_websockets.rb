7dbd796cca3e0ea10f23515930e66b873dbcfff9
Mikhail <mikes22@yandex.ru>
2020-04-22 14:30:33 +0300

    Reorganise and fix the template of Rb WS Server

--- Lab06/server_websockets_rb/server_websockets.rb
+++ Lab06/server_websockets_rb/server_websockets.rb
@@ -3,22 +3,38 @@
 
 require 'em-websocket'
 
+# temporal "monkey patch"
+class EM::WebSocket::Connection
+  def remote_ip
+    get_peername[2, 6].unpack('nC4')[1..4].join('.')
+  end
+end
+
+def process_msg(msg, wsc)
+  puts "msg from #{wsc.remote_ip}:#{msg}"
+  words = msg.split(' ')
+  
+  answer = (eval words[1] + words[0] + words[2]).to_s + "\r\n"
+  wsc.send answer
+  puts 'Answer: ' + answer
+end
+
 EM.run do
-  @concurrentClientCount = 0
-  EM::WebSocket.run(:host => "0.0.0.0", :port => 28563) do |ws|
+  @concurrent_client_count = 0
+  EM::WebSocket.run(host: '0.0.0.0', port: 28563) do |ws|
     ws.onopen do |handshake|
-      @concurrentClientCount += 1
-      puts "#{@concurrentClientCount} concurrent clients are connected"
+      @concurrent_client_count += 1
+      puts "#{@concurrent_client_count} concurrent clients are connected"
     end
 
-    ws.onclose { @concurrentClientCount -= 1 }
+    ws.onclose { @concurrent_client_count -= 1 }
 
     ws.onmessage do |query|
-      puts "Data received: "+query
-      words = query.split(" ")
-      answer = (eval words[1]+words[0]+words[2]).to_s + "\r\n"
-      ws.send answer
-      puts "Answer: "+answer
+      ProcessMsg(query, ws)
     end
   end
 end
+
+
+
+
